1fa7b478de49f53e3a0de78e808ca229145b4f25
Gabriel-2310 <luisgamorales1@gmail.com>
2022-08-06 21:19:21 -0500

    implementacion del nabvar, menu amburguesa responsive

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import About from "./Components/About/index";
 import Contact from "./Components/Contact/index";
 import Proyects from "./Components/Proyects/index";
 import Welcome from "./Components/Welcome/index";
+import Navbar from "./Components/Navbar";
 import "./app.css";
+
 
 function App() {
   return (
     <React.Fragment>
+      <Route path={"/"} component={Navbar}/>
       <Route path={"/about"} component={About}/>
       <Route path={"/contact"} component={Contact}/>
       <Route path={"/proyects"} component={Proyects}/>
-      <Route exact path={"/"} component={Welcome}/>
+      <Route exact path={"/welcom"} component={Welcome}/>      
     </React.Fragment>
   );
 }
---
+++ src/Components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import "./index.css";
+
+const Navbar = () => {
+    const [show, setshow] = useState(false);
+
+    function Show (e){
+        e.preventDefault();
+        show? setshow(false) : setshow(true)         
+      }
+      
+    return (
+        <header>
+            <nav className='menu'>
+                <label className='logo'>LuisGabriel</label>             
+                <ul className={show? "show menu-items":'menu-items'}>
+                    <li><a className='active' href='/home'>home </a></li>
+                    <li><a href="/proyects">Proyects</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+                <btn onClick={Show} className='btn-menu'>
+                    <FontAwesomeIcon icon={faBars} /> 
+                </btn>                
+            </nav>            
+        </header>
+    )
+};
+
+export default Navbar;
